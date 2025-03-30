477db4285dcf183a98d6eeb0da53c65968ae673e
wwweert123 <puachengxuan.fp@gmail.com>
2025-03-30 17:57:23 +0800

    init assignment 4

---
+++ udp_client4.c
@@ -0,0 +1,162 @@
+/*******************************
+tcp_client.c: the source file of the client in tcp transmission 
+********************************/
+
+#include "headsock.h"
+
+float str_cli(FILE *fp, int sockfd, struct sockaddr_in *serv_addr, socklen_t addr_len, long *len);                       //transmission function
+void tv_sub(struct  timeval *out, struct timeval *in);	    //calcu the time interval between out and in
+void print_host_info(struct hostent *sh);
+int setup_socket(struct sockaddr_in *ser_addr, struct in_addr **addrs);
+FILE *open_file(const char *filename);
+
+int main(int argc, char **argv)
+{
+	if (argc != 2) {
+		printf("Usage: %s <hostname>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+    struct hostent *sh = gethostbyname(argv[1]);
+    if (sh == NULL) {
+        perror("Error: Unable to resolve hostname");
+        exit(EXIT_FAILURE);
+    }
+
+	print_host_info(sh);
+
+	struct sockaddr_in ser_addr;
+    struct in_addr **addrs = (struct in_addr **)sh->h_addr_list;
+
+    int sockfd = setup_socket(&ser_addr, addrs);
+    FILE *fp = open_file("myfile.txt");
+
+	// Start transmission
+	long len;
+	float ti = str_cli(fp, sockfd, &ser_addr, sizeof(struct sockaddr), &len);
+	float rt = (len/(float)ti);
+
+	printf("Time(ms) : %.3f, Data sent(byte): %d\nData rate: %f (Kbytes/s)\n", ti, (int)len, rt);
+
+	close(sockfd);
+	fclose(fp);
+
+	exit(EXIT_SUCCESS);
+}
+
+// Open and return the file pointer
+FILE *open_file(const char *filename)
+{
+    FILE *fp = fopen(filename, "r+t");
+    if (fp == NULL) {
+        perror("Error: File doesn't exist");
+        exit(EXIT_FAILURE);
+    }
+    return fp;
+}
+
+// Setup and return the socket descriptor
+int setup_socket(struct sockaddr_in *ser_addr, struct in_addr **addrs)
+{
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("Error creating socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Configure server address
+    ser_addr->sin_family = AF_INET;
+    ser_addr->sin_port = htons(MYTCP_PORT);
+    memcpy(&(ser_addr->sin_addr.s_addr), *addrs, sizeof(struct in_addr));
+    bzero(&(ser_addr->sin_zero), 8);
+
+    return sockfd;
+}
+
+// Print host information
+void print_host_info(struct hostent *sh)
+{
+    printf("Canonical name: %s\n", sh->h_name);
+    for (char **pptr = sh->h_aliases; *pptr != NULL; pptr++) {
+        printf("Alias: %s\n", *pptr);
+    }
+
+    switch (sh->h_addrtype) {
+        case AF_INET:
+            printf("Address type: AF_INET\n");
+            break;
+        default:
+            printf("Unknown address type\n");
+            break;
+    }
+}
+
+float str_cli(FILE *fp, int sockfd, struct sockaddr_in *serv_addr, socklen_t addr_len, long *len)
+{
+	long lsize, ci = 0;
+	int n, slen;
+	char *buf, sends[DATALEN];
+
+	struct ack_so ack;
+	struct timeval sendt, recvt;
+
+	// Get File size and read into buffer
+	fseek (fp , 0 , SEEK_END);
+	lsize = ftell (fp);
+	rewind (fp);
+	printf("The file length is %d bytes\n", (int)lsize);
+	printf("the packet length is %d bytes\n",DATALEN);
+
+	// allocate memory to contain the whole file.
+	buf = (char *)malloc(lsize);
+	if (buf == NULL) {
+		perror("Error: Memory allocation failed");
+		exit(EXIT_FAILURE);
+	}
+
+	fread (buf, 1, lsize, fp);
+	buf[lsize] = '\0';
+
+	gettimeofday(&sendt, NULL);
+	while(ci<= lsize) {
+        slen = ((lsize + 1 - ci) <= DATALEN) ? (lsize + 1 - ci) : DATALEN;
+        memcpy(sends, (buf + ci), slen);
+
+		n = sendto(sockfd, &sends, slen, 0, (struct sockaddr *)serv_addr, addr_len);
+		if(n == -1) {
+			printf("send error!");
+			free(buf);
+			exit(EXIT_FAILURE);
+		}
+		ci += slen;
+	}
+
+	n = recvfrom(sockfd, &ack, 2, 0, (struct sockaddr *)serv_addr, &addr_len);
+	if (n == -1) {
+		printf("error when receiving\n");
+		exit(1);
+	}
+
+	if (ack.num != 1|| ack.len != 0) {
+		printf("error in transmission\n");
+	}
+
+	gettimeofday(&recvt, NULL);
+	*len= ci;
+	tv_sub(&recvt, &sendt);
+
+	float time_inv = (recvt.tv_sec)*1000.0 + (recvt.tv_usec)/1000.0;
+	free(buf);
+
+	return(time_inv);
+}
+
+void tv_sub(struct  timeval *out, struct timeval *in)
+{
+	if ((out->tv_usec -= in->tv_usec) < 0)
+	{
+		--out ->tv_sec;
+		out ->tv_usec += 1000000;
+	}
+	out->tv_sec -= in->tv_sec;
+}
---
+++ udp_ser4.c
@@ -0,0 +1,146 @@
+/**********************************
+tcp_ser.c: the source file of the server in tcp transmission 
+***********************************/
+
+
+#include "headsock.h"
+
+#define BACKLOG 10
+
+void str_ser(int sockfd);
+int setup_server_socket(struct sockaddr_in *my_addr);
+void receive_file(int sockfd, struct sockaddr_in *client_addr, socklen_t addr_len);
+int verify_file();
+
+int main(void)
+{
+	struct sockaddr_in my_addr;
+	int sockfd = setup_server_socket(&my_addr);
+
+	while (1) {
+		str_ser(sockfd);
+	}
+
+	close(sockfd);
+	return 0;
+}
+
+// Set up the UDP server socket and return the descriptor
+int setup_server_socket(struct sockaddr_in *my_addr)
+{
+    int sockfd, ret;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0); // Create UDP socket
+    if (sockfd < 0) {
+        perror("Error creating socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Configure server address
+    my_addr->sin_family = AF_INET;
+    my_addr->sin_port = htons(MYTCP_PORT);
+    my_addr->sin_addr.s_addr = htonl(INADDR_ANY);
+    bzero(&(my_addr->sin_zero), 8);
+
+    // Bind socket to the address and port
+    ret = bind(sockfd, (struct sockaddr *)my_addr, sizeof(struct sockaddr));
+    if (ret < 0) {
+        perror("Error binding socket");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server is ready to receive data...\n");
+    return sockfd;
+}
+
+void str_ser(int sockfd) {
+	struct sockaddr_in client_addr;
+	socklen_t addr_len = sizeof(struct sockaddr_in);
+	
+	receive_file(sockfd, &client_addr, addr_len);
+
+	if (verify_file()) {
+        printf("✅ Verification successful! Files are identical.\n");
+    } else {
+        printf("❌ Verification failed. Files are not the same.\n");
+    }
+}
+
+// Receive file data and save to file
+void receive_file(int sockfd, struct sockaddr_in *client_addr, socklen_t addr_len)
+{
+    char buf[BUFSIZE];
+    char recvs[DATALEN];
+    struct ack_so ack;
+    FILE *fp;
+    int n = 0, end = 0;
+    long lseek = 0;
+
+    printf("Receiving data...\n");
+
+    // Receive data until the end of the file
+    while (!end) {
+        n = recvfrom(sockfd, &recvs, DATALEN, 0, (struct sockaddr *)client_addr, &addr_len);
+        if (n == -1) {
+            perror("Error receiving data");
+            exit(EXIT_FAILURE);
+        }
+
+        // Check if it's the end of the file
+        if (recvs[n - 1] == '\0') {
+            end = 1;
+            n--; // Ignore the end byte
+        }
+
+        memcpy((buf + lseek), recvs, n);
+        lseek += n;
+    }
+
+    // Send acknowledgment
+    ack.num = 1;
+    ack.len = 0;
+    if (sendto(sockfd, &ack, 2, 0, (struct sockaddr *)client_addr, addr_len) == -1) {
+        printf("Error sending acknowledgment");
+        exit(EXIT_FAILURE);
+    }
+
+    // Write received data to file
+    if ((fp = fopen("myUDPreceive.txt", "wt")) == NULL) {
+        printf("Error opening file for writing");
+        exit(EXIT_FAILURE);
+    }
+
+    fwrite(buf, 1, lseek, fp);
+    fclose(fp);
+
+    printf("File successfully received!\nTotal data received: %ld bytes\n", lseek);
+}
+
+// Verify if two files are identical
+int verify_file()
+{
+	const char *received_file = "myUDPreceive.txt";
+    const char *original_file = "myfile.txt";
+
+    FILE *fp1 = fopen(received_file, "rt");
+    FILE *fp2 = fopen(original_file, "rt");
+
+    if (fp1 == NULL || fp2 == NULL) {
+        perror("Error opening files for verification");
+        if (fp1) fclose(fp1);
+        if (fp2) fclose(fp2);
+        return 0; // Verification failed
+    }
+
+    char ch1, ch2;
+    while (((ch1 = fgetc(fp1)) != EOF) && ((ch2 = fgetc(fp2)) != EOF)) {
+        if (ch1 != ch2) {
+            fclose(fp1);
+            fclose(fp2);
+            return 0; // Files do not match
+        }
+    }
+
+    return 1;
+}
